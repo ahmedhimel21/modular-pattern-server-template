516276141c430bfc8a39e494e69f2fce60b70101
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-05-20 19:38:54 +0600

    product interface created

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,22 @@
+// variants type
+export type TVariants = {
+  type: string;
+  value: string;
+};
+
+// inventory type
+export type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+// product type
+export type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: [TVariants];
+  inventory: TInventory;
+};
