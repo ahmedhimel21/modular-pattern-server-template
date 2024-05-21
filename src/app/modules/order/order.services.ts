5e57cbc63871f6eab262d4ef772d0b07a5317cab
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-05-21 14:11:10 +0600

    Inventory Management Logic and not found route implemented

--- src/app/modules/order/order.services.ts
+++ src/app/modules/order/order.services.ts
@@ -1,9 +1,32 @@
 import Order from "../order.model";
+import Product from "../product.model";
 import { TOrder } from "./order.interface";
 
 // create order
 const createOrderIntoDB = async (order: TOrder) => {
+  // create order
   const result = await Order.create(order);
+  // find order
+  const product = await Product.findOne({ _id: order.productId });
+  // product not found validation
+  if (!product) {
+    throw new Error("Product not found");
+  }
+  // available quantity validation
+  const availableQuantity = product.inventory.quantity;
+  if (order.quantity > availableQuantity) {
+    throw new Error("Insufficient quantity available in inventory");
+  }
+  // update inventory
+  await Product.updateOne(
+    { _id: order.productId },
+    { $inc: { "inventory.quantity": -order.quantity } }
+  );
+  // update stock status
+  await Product.updateOne(
+    { _id: order.productId, "inventory.quantity": { $eq: 0 } },
+    { $set: { "inventory.inStock": false } }
+  );
   return result;
 };
 
